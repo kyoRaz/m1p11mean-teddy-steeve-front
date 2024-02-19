eaf7d6a2061aa1bdaf1262dfaeffb72736f5d7d4
Teddy_Brown <teddylong132@gmail.com>
2024-02-19 15:03:33 +0300

    Add register

--- src/app/pages/authentication/register/register.component.ts
+++ src/app/pages/authentication/register/register.component.ts
@@ -1,26 +1,38 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HttpService } from '../../../services/http/http.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
 })
 export class AppSideRegisterComponent {
-  constructor(private router: Router) {}
+  constructor(private httpservice: HttpService,private router: Router) {}
 
-  form = new FormGroup({
-    uname: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    email: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
+  servicename: string = "beauty/users/client";
+  formData = new FormGroup({
+    nom: new FormControl('', [Validators.required]),
+    prenom: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required,Validators.minLength(8)]),
   });
 
   get f() {
-    return this.form.controls;
+    return this.formData.controls;
   }
 
-  submit() {
-    // console.log(this.form.value);
-    this.router.navigate(['/dashboard']);
+  OnSubmit() {
+    console.log('Formulaire soumis :', this.formData.value);
+    if(this.formData.valid){
+      console.log(this.formData.value)
+      this. insertData();
+    }
+    // this.router.navigate(['/authentication/register']);
+  }
+  async insertData(){
+    (await this.httpservice.postbod(this.servicename, this.formData.value)).subscribe(()=>{
+      this.router.navigate(['/authentication/login']);
+    });
   }
 }
