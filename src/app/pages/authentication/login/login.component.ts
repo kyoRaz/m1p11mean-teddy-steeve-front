8f5a642841637ccb4aefd51f49daeac6319aa24b
Kyo <kyostvraz@gmail.com>
2024-02-20 13:35:08 +0300

    feat  :  Integration  Login

--- src/app/pages/authentication/login/login.component.ts
+++ src/app/pages/authentication/login/login.component.ts
@@ -1,9 +1,46 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/auth/login.service';
+import { LocalStorageService } from 'src/app/services/storage/local-storage.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
 export class AppSideLoginComponent {
-  constructor() {}
+
+  formData: any ={};
+
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private localStorageService: LocalStorageService
+    ) {}
+
+  onSubmit(formData: any) {
+    this.formData=formData;
+    this.loginService.authToAPI(this.formData).subscribe(response => {
+      console.log('Réponse de l\'API :', response);
+      this.localStorageService.saveData('token',response.token);
+      this.localStorageService.saveData('user',response.user);
+
+        // Redirection par  Role
+        const userRole = response.user.roleId.libelle;
+
+        if (userRole === 'manager') {
+          this.router.navigate(['/manager']);
+        } else if (userRole === 'employe') {
+          this.router.navigate(['/employe']); 
+        } else if (userRole === 'client') {
+          this.router.navigate(['/client']); 
+        } else {
+          console.error('Rôle d\'utilisateur non reconnu :', userRole);
+          this.router.navigate(['/error']); 
+        }
+
+    }, error => {
+      console.error('Erreur lors de la requête POST :', error);
+    });
+  }
+
 }
