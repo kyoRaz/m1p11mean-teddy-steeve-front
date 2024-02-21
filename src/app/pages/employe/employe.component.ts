b27181a3152ed9546d189a06346dd274c5194f7c
Kyo <kyostvraz@gmail.com>
2024-02-21 22:37:49 +0300

    feat :  horaire et Profil  Employe

--- src/app/pages/employe/employe.component.ts
+++ src/app/pages/employe/employe.component.ts
@@ -12,6 +12,7 @@ export class EmployeComponent {
 
   formData: any = {};
   user: any = {};
+  horaire: any = {};
   servicename: string = "users";
   idUser: string = "65bf662353006be666fda322"
 
@@ -21,13 +22,29 @@ export class EmployeComponent {
     this.httpService.getOne(this.servicename, this.idUser).subscribe(response => {
       this.user = response;
     });
-    console.log(this.user);
+    this.httpService.getOne("horaires/employe", this.idUser).subscribe(response => {
+      this.horaire = response.result;
+      console.log(this.horaire);
+    });
   }
 
   async onSubmit(formData: any) {
-    console.log(formData);
     let url = "beauty/" + this.servicename + "/" + this.idUser;
 
+    (await this.httpService.putData(url, formData)).subscribe(
+      (response: any) => {
+        alert("Success");
+      },
+      (error: any) => {
+        console.error(error);
+        alert("Une erreur s'est produite : " + error.message);
+      }
+    );
+  }
+
+
+  async updateHoraire(formData: any) {
+    let url = "beauty/horaires/" + this.horaire._id;
     (await this.httpService.putData(url, formData)).subscribe(
       (response: any) => {
         alert("Success");
