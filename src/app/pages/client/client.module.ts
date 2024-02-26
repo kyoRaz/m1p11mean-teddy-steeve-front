25df4158a4004a12f7392cd9fddd3b3d3dc9322b
Teddy_Brown <teddylong132@gmail.com>
2024-02-26 15:14:55 +0300

    clean code

--- src/app/pages/client/client.module.ts
+++ src/app/pages/client/client.module.ts
@@ -16,7 +16,8 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
   declarations: [
     PreferenceComponent,
     HistoriqueComponent,
-    RdvComponent,AppTakerdvComponent
+    RdvComponent,
+    AppTakerdvComponent
   ],
   imports: [
     CommonModule,
