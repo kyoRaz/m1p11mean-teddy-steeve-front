0d255097a10900cc234e516392a321ea6accc14e
Kyo <kyostvraz@gmail.com>
2024-02-29 23:54:35 +0300

    feat : notification  Reussi

--- src/app/pages/client/createrdv/takerdv.component.ts
+++ src/app/pages/client/createrdv/takerdv.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { HttpService } from '../../../services/http/http.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialog,MatDialogActions,MatDialogClose,MatDialogContent,MatDialogTitle} from '@angular/material/dialog';
+import { MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
 import { AppDialogComponent } from '../dialog/dialog.component';
 import { LoaderService } from 'src/app/services/loader/loader.service';
+import { AlertService } from 'src/app/services/alert/alert.service';
 
 // ecommerce card
 interface productcards {
@@ -18,7 +19,7 @@ interface productcards {
   selector: 'app-tache',
   templateUrl: './takerdv.component.html',
 })
-export class AppTakerdvComponent implements OnInit{
+export class AppTakerdvComponent implements OnInit {
 
 
   servicename: string = "services";
@@ -31,50 +32,57 @@ export class AppTakerdvComponent implements OnInit{
   });
   rdv: any = '';
   firstFormGroup = this.formData;
-  secondFormGroup :any  ;
+  secondFormGroup: any;
   isLinear = false;
   isRDV = false;
   dataSource: any;
   listrdv: any = {
-    dateRdv:'',
-    heureRdv:'',
-    listDetails:[]
+    dateRdv: '',
+    heureRdv: '',
+    listDetails: []
   }
 
-  constructor(private httpservice: HttpService,public dialog: MatDialog
-    ,public loader: LoaderService) { }
+  constructor(private httpservice: HttpService, public dialog: MatDialog
+    , public loader: LoaderService,
+    public sweet: AlertService) { }
 
   ngOnInit(): void {
     this.loadData()
   }
   openDialog(service: any) {
-    this.dialog.open(AppDialogComponent,{
-      data: {date: this.rdv,service: service,list: this.listrdv.listDetails,isfull :this.isRDV}
+    this.dialog.open(AppDialogComponent, {
+      data: { date: this.rdv, service: service, list: this.listrdv.listDetails, isfull: this.isRDV }
     });
   }
-  loadData(){
-    this.httpservice.get(this.servicename).subscribe((data: any) =>{
-      if(data){
-        this.dataSource=data.resultat;
+  loadData() {
+    this.httpservice.get(this.servicename).subscribe((data: any) => {
+      if (data) {
+        this.dataSource = data.resultat;
         this.formData.reset();
       }
       console.log(data);
-    },(error: any) =>{
-        console.error('An error has occured:',error);
+    }, (error: any) => {
+      console.error('An error has occured:', error);
     });
   }
-  async deal(stepper: any){
-    (await this.httpservice.postbod(this.servicenameRDV, this.listrdv)).subscribe(()=>{
-      stepper.reset();
-    });
+  async deal(stepper: any) {
+    (await this.httpservice.postbod(this.servicenameRDV, this.listrdv)).subscribe(
+      (response) => {
+        this.sweet.showSuccessAlert("préférence enregistré");
+        stepper.reset();
+      },
+      (error) => {
+        this.sweet.showErrorAlert("Erreur lors  de la transaction");
+      }
+    );;
   }
-  onSubmit(){
-    if(this.formData.valid){
-      
+  onSubmit() {
+    if (this.formData.valid) {
+
       this.rdv = this.formData.get('date')?.value;
       this.formData.reset();
-      this.listrdv.dateRdv=this.rdv.split('T')[0];
-      this.listrdv.heureRdv=this.rdv.split('T')[1]+":00";
+      this.listrdv.dateRdv = this.rdv.split('T')[0];
+      this.listrdv.heureRdv = this.rdv.split('T')[1] + ":00";
       console.log(this.listrdv);
       this.scrollToSection('rdv');
     }
@@ -90,6 +98,6 @@ export class AppTakerdvComponent implements OnInit{
     return this.formData.dirty;
   }
 
-  
+
 
 }
