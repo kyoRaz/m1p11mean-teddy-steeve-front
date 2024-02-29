8359de16a8d7f89258831347caca96643c3624f8
Teddy_Brown <81391297+Brown-Dohn@users.noreply.github.com>
2024-02-29 21:14:53 +0300

    fix solde

--- src/app/pages/client/sodl/sodl.component.ts
+++ src/app/pages/client/sodl/sodl.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { HttpService } from 'src/app/services/http/http.service';
 
+export interface Section {
+  name: string;
+  updated: Date;
+}
 @Component({
   selector: 'app-sodl',
   templateUrl: './sodl.component.html',
@@ -8,12 +13,21 @@ import { HttpService } from 'src/app/services/http/http.service';
 })
 export class SodlComponent implements OnInit{
   
+  solde : any;
+  formData = new FormGroup({
+    _id: new FormControl(''),
+    solde: new FormControl('', [Validators.required])
+  });
   constructor(private httpService: HttpService){
 
   }
-
+  
   ngOnInit(): void {
     
   }
+  onSubmit(){
+    
+  }
 
+  
 }
